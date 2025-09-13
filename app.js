7102d233d801cc3e7692a26e6ae497d52d64c668
Frandalor <f.andal.git@gmail.com>
2025-09-13 18:04:20 +0200

    added basic security (helmet)

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const localStrategy = require('passport-local');
 const User = require('./models/user');
+const sanitizeV5 = require('./utils/mongoSanitizeV5');
+const helmet = require('helmet');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -27,6 +29,7 @@ db.once('open', () => {
 
 const app = express();
 
+app.set('query parser', 'extended');
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -34,19 +37,61 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(sanitizeV5({ replaceWith: '_' }));
 
 const sessionConfig = {
+  name: 'session',
   secret: 'thisshouldbeabettersecret',
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true,  // only works on https
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+  'https://stackpath.bootstrapcdn.com/',
+  'https://kit.fontawesome.com/',
+  'https://cdnjs.cloudflare.com/',
+  'https://cdn.jsdelivr.net',
+  'https://cdn.maptiler.com/',
+];
+const styleSrcUrls = [
+  'https://kit-free.fontawesome.com/',
+  'https://stackpath.bootstrapcdn.com/',
+  'https://use.fontawesome.com/',
+  'https://cdn.jsdelivr.net',
+  'https://cdn.maptiler.com/',
+];
+const connectSrcUrls = ['https://api.maptiler.com/'];
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        'blob:',
+        'data:',
+        'https://res.cloudinary.com/deawyqfb9/',
+        'https://images.unsplash.com/',
+        'https://api.maptiler.com/',
+      ],
+      fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+    },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
