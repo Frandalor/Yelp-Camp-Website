72913f68a42ac3cb6925d4db2f24ce31e87187a4
Frandalor <f.andal.git@gmail.com>
2025-09-15 13:38:30 +0200

    port change

--- app.js
+++ app.js
@@ -145,6 +145,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-  console.log('Port 3000 OPENED');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
 });
